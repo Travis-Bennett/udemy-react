508628e36eb6a18d043b09399d956d0e4c71a584
Travis-Bennett <bennett.travis.b@gmail.com>
2022-07-01 13:34:19 -0400

    This is the beginnings of making my Etch-A-Sketch game. We created the board using css grid and have added a hover effect over the squares. Still need to add an effect to change tthe size of the board and make the hover effects permanent by using javascript.

---
+++ src/components/E-A-S Components/GridSizeBtn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GridSizeBtn = () =>(
+    <div>
+        <h1>Please Enter in the number the size of your art (4 will be a 4x4 grid)</h1>
+        <input type="text" />
+        <button type="submit">Create Grid</button>
+    </div>
+    
+)
+export default GridSizeBtn;
---
+++ src/components/E-A-S Components/GridSquare.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const GridSquare = props =>(
+    <div className="square">
+        
+    </div>
+)
+
+export default GridSquare;
